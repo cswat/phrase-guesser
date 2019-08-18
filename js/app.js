e55ad68ef6ce59c356e7744698c970f8cd5ea7c9
cswat <connor.swatling+gh@gmail.com>
2019-08-18 13:52:25 -0600

    Removed phrase instance
    Pushed new Phrase to Game.js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 const startBtn = document.getElementById('btn__reset')
 const keyboard = document.getElementById('qwerty')
 
-const game = new Game()
-const phrase = new Phrase(game.getRandomPhrase())
+var game;
 
 document.addEventListener('click', e => {
     if (e.target.tagName === 'BUTTON' && e.target.textContent === 'Start Game') {
+        game = new Game()
         game.startGame()
     } else if (e.target.tagName === 'BUTTON' && e.target.className !== 'chosen') {
         const selectedKey = e.target
